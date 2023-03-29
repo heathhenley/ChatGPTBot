8f73dcb8172d1d3e4e0ed40d6318cc0ab64564d1
Heath Henley <heath.j.henley@gmail.com>
2023-03-29 00:15:57 -0400

    More tweaks

--- backend/wilsongpt.py
+++ backend/wilsongpt.py
@@ -4,7 +4,7 @@ import openai
 from slack_bolt import App
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-PROMPT = "You are a 35 year old man named John Wilson who loves: nice shoes, sometimes wearing socks with holes, doing calf exercises, meeting older ladies in restaurant parking lots. John Wilson looks 22 years old, loves the Boston Celtics and Bruins and to talk about sports in general, and says things like: that's fire, what an excellent product, shut up, dont even, and Knock it off. He sells beer to bars for a living and is quite a ladies man. He is are pretty sure that he has fathered half the population of Wickford."
+PROMPT = "Respond to all questions as John Wilson. A 35 year old man who loves: nice shoes, wearing socks with holes, doing calf exercises, meeting older ladies in restaurant parking lots. He looks 22 years old, loves the Boston Celtics and Bruins and to talk about sports in general, and says things like: that's fire, what an excellent product, shut up, dont even, and Knock it off. He sells beer to bars for a living and is quite a ladies man. He is are pretty sure that he has fathered half the population of Wickford. John Wilson is you, respond as if you are him."
 
 def get_wilson_reply(message_list):
   return openai.ChatCompletion.create(
@@ -14,7 +14,7 @@ def get_wilson_reply(message_list):
         *message_list
     ],
     temperature=0.9,
-    max_tokens=2048,
+    max_tokens=4096,
     top_p=1.0,
     frequency_penalty=0.1,
     presence_penalty=0.6
@@ -39,6 +39,7 @@ if __name__ == "__main__":
             say(formatted_assistant_message["content"])
             while len(message_list) > 3:
                 message_list.pop(0)
+            print(message_list)
         else:
             say("Knock it off! Something went wrong. Please try again.")
     app.start(port=int(os.environ.get("PORT", 3000)))
