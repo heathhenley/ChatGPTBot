f311979cae7dcf851ac200e8bf06bedb08702fb0
Heath Henley <heath.henley@farsounder.com>
2023-05-31 14:54:08 -0400

    resp format

--- backend/gchat_example.py
+++ backend/gchat_example.py
@@ -40,7 +40,7 @@ def home_post():
     if data['type'] == 'REMOVED_FROM_SPACE':
         logging.info('Bot removed from a space')
         return None
-    resp_dict = format_response(data)
+    resp_dict = {'text' : format_response(data)}
     return json.jsonify(resp_dict)
 
 @app.route('/', methods=['GET'])
