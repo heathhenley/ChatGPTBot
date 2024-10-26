77405236448087668c7946471fc41941ed6e9545
Heath Henley <heath.j.henley@gmail.com>
2024-10-26 17:35:06 -0400

    value can be None now
    not sure why but tbd

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -11,8 +11,8 @@ def num_tokens(messages, model, prompt=""):
     for message in messages:
         num_tks += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
         for key, value in message.items():
-            print(value, type(value))
-            num_tks += len(encoding.encode(value))
+            if value:
+                num_tks += len(encoding.encode(value))
             if key == "name":  # if there's a name, the role is omitted
                 num_tks += -1  # role is always required and always 1 token
     num_tks += 2  # every reply is primed with <im_start>assistant
